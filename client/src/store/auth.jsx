7bf9785e0b74eed87728dfb726aed8ab6845f8aa
Krish-Lathidadiya <krishlathidadiya.sus@gmail.com>
2024-05-15 14:39:37 +0530

    we also write auth like this

--- client/src/store/auth.jsx
+++ client/src/store/auth.jsx
@@ -47,3 +47,81 @@ export const useAuth = () => {
 
   return useContextValue;
 };
+
+
+// import React, { createContext, useContext, useState, useEffect } from "react";
+// import { auth } from "../firebase/config";
+// import { onAuthStateChanged } from "firebase/auth";
+// import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+// export const AuthContext = createContext();
+
+// export const AuthProvider = ({ children }) => {
+//   const [user, setUser] = useState(null);
+//   const [loading, setLoading] = useState(true);
+//   const db = getFirestore();
+
+//   useEffect(() => {
+//     const fetchUserData = async (user) => {
+//       //same as findById
+//       const userDoc = await getDoc(doc(db, "users", user.uid));
+//       if (userDoc) {
+//         const userData = userDoc.data();
+//         console.log("User logged in:", {
+//           uid: user.uid,
+//           email: user.email,
+//           role: userData.role,
+//         });
+//         setUser({
+//           uid: user.uid,
+//           email: user.email,
+//           role: userData.role,
+//         });
+//       } else {
+//         // Initialize user data if it doesn't exist
+//         const defaultRole = "user";
+//         await setDoc(doc(db, "users", user.uid), {
+//           email: user.email,
+//           role: defaultRole,
+//         });
+//         console.log("User logged in and additional data initialized:", {
+//           uid: user.uid,
+//           email: user.email,
+//           role: defaultRole,
+//         });
+//         setUser({
+//           uid: user.uid,
+//           email: user.email,
+//           role: defaultRole,
+//         });
+//       }
+//       setLoading(false);
+//     };
+
+//     const unsubscribe = onAuthStateChanged(auth, (user) => {
+//       if (user) {
+//         fetchUserData(user);
+//       } else {
+//         setUser(null);
+//         setLoading(false);
+//       }
+//     });
+
+//     return () => unsubscribe();
+//   }, [db]);
+
+//   const isLoggedIn = !!user;
+//   return (
+//     <AuthContext.Provider value={{ isLoggedIn, user }}>
+//       {!loading && children}
+//     </AuthContext.Provider>
+//   );
+// };
+
+// export const useAuth = () => {
+//   const contextValue = useContext(AuthContext);
+//   if (!contextValue) {
+//     throw new Error("useAuth must be used within an AuthProvider");
+//   }
+//   return contextValue;
+// };
